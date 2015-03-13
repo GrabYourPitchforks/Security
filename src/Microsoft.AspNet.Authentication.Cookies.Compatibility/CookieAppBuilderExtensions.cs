1f4caee2750d2091d6ccedec2991a57ef07236d6
Levi B <levib@yahoo.com>
2015-03-12 19:13:52 -0700

    Latest updates

--- src/Microsoft.AspNet.Authentication.Cookies.Compatibility/CookieAppBuilderExtensions.cs
+++ src/Microsoft.AspNet.Authentication.Cookies.Compatibility/CookieAppBuilderExtensions.cs
@@ -3,6 +3,7 @@
 
 using System;
 using Microsoft.AspNet.Authentication.Cookies.Compatibility;
+using Microsoft.AspNet.DataProtection.SystemWeb;
 using Microsoft.Framework.Internal;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.DataProtection;
@@ -15,19 +16,60 @@ namespace Owin
             [NotNull] this IAppBuilder app,
             [NotNull] CookieAuthenticationOptions options,
             PipelineStage stage = PipelineStage.Authenticate,
-            bool createShareableTickets = false)
+            bool createShareableTickets = false,
+            string authenticationScheme = null)
         {
             // If we're asked to create shareable tickets, then we need to inject our own ticket formatter
             // that's compatible with ASP.NET 5.
             if (createShareableTickets)
             {
+                // In ASP.NET 5 cookie middleware and identity, there's a distinction between auth scheme
+                // (of which there's one per cookie / ticket) and auth type (of which there's one per identity).
+                // We'll try to perform auto-fixup here by using the defaults.
+                if (authenticationScheme == null)
+                {
+                    if ((options.AuthenticationType?.EndsWith(".AuthType", StringComparison.OrdinalIgnoreCase)).GetValueOrDefault())
+                    {
+                        authenticationScheme = options.AuthenticationType.Substring(0, options.AuthenticationType.Length - ".AuthType".Length);
+                    }
+                    else
+                    {
+                        authenticationScheme = options.AuthenticationType;
+                    }
+                }
+
                 IDataProtector dataProtector = app.CreateDataProtector(
                     "Microsoft.AspNet.Authentication.Cookies.CookieAuthenticationMiddleware", // full name of the ASP.NET 5 type
-                    options.AuthenticationType, "v2");
-                options.TicketDataFormat = new AspNet5TicketDataFormat(dataProtector, options.AuthenticationType);
+                    authenticationScheme, "v2");
+                dataProtector = new WrappingDataProtector(dataProtector);
+                options.TicketDataFormat = new AspNet5TicketDataFormat(dataProtector, authenticationScheme);
             }
 
             return app.UseCookieAuthentication(options, stage);
+        }
+
+        private sealed class WrappingDataProtector : IDataProtector
+        {
+            private readonly IDataProtector _innerProtector;
+
+            public WrappingDataProtector(IDataProtector innerProtector)
+            {
+                _innerProtector = innerProtector;
+            }
+
+            public byte[] Protect(byte[] userData)
+            {
+                return CompatibilityDataProtector.RunWithSuppressedPrimaryPurpose(
+                    (@this, input) => ((IDataProtector)@this).Protect(input),
+                    _innerProtector, userData);
+            }
+
+            public byte[] Unprotect(byte[] protectedData)
+            {
+                return CompatibilityDataProtector.RunWithSuppressedPrimaryPurpose(
+                    (@this, input) => ((IDataProtector)@this).Unprotect(input),
+                    _innerProtector, protectedData);
+            }
         }
     }
 }
